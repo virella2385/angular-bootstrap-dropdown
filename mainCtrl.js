39d17f3e7e5d08fbdcb5814d947bb9d2154c96bc
Chris <alakratt@gmail.com>
2017-05-08 19:39:01 -0400

    beautified it

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,14 +1,33 @@
 angular.module('app')
-  .controller('mainCtrl', ['$http', '$scope', function ($http, $scope){
+     .controller('mainCtrl', ['$http', '$scope', function($http, $scope) {
 
-    //$scope.selected = 'default';
-    $scope.name = 'chris';
-    //$scope.array = [];
-    $scope.array = [{name: 'chris', value: 1}, {name: 'bruce', value: 2}, {name: 'clark', value: 3},
-    {name: 'arthur', value: 4}, {name: 'diana', value: 5}];
+          //$scope.selected = 'default';
+          $scope.name = 'chris';
+          //$scope.array = [];
+          $scope.array = [{
+                    name: 'chris',
+                    value: 1
+               }, {
+                    name: 'bruce',
+                    value: 2
+               }, {
+                    name: 'clark',
+                    value: 3
+               },
+               {
+                    name: 'arthur',
+                    value: 4
+               }, {
+                    name: 'diana',
+                    value: 5
+               }
+          ];
 
-    $scope.testfn = function (){
-      console.log('called controller function!');
-    };
+          $scope.testfn = function() {
+               console.log('called controller function!');
+          };
 
-  }]);
+
+
+
+     }]);
